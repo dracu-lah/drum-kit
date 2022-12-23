91b877c795e89677b1f2ab81df32f5f4a5f97bea
nevilkrishna <nevilkrishna@gmail.com>
2022-12-23 21:59:29 +0530

    unminified

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-let drums=document.querySelectorAll(".drum");function keyCheck(e){switch(e){case"a":new Audio("./sounds/crash.mp3").play();break;case"s":new Audio("./sounds/kick-bass.mp3").play();break;case"d":new Audio("./sounds/snare.mp3").play();break;case"f":new Audio("./sounds/tom-1.mp3").play();break;case"j":new Audio("./sounds/tom-2.mp3").play();break;case"k":new Audio("./sounds/tom-3.mp3").play();break;case"l":new Audio("./sounds/tom-4.mp3").play()}}for(let i=0;i<drums.length;i++)drums[i].addEventListener("click",function(){keyCheck(this.innerHTML)});document.addEventListener("keydown",function(e){keyCheck(e.key)});
+let drums = document.querySelectorAll(".drum");
+function keyCheck(e) {
+  switch (e) {
+    case "a":
+      new Audio("./sounds/crash.mp3").play();
+      break;
+    case "s":
+      new Audio("./sounds/kick-bass.mp3").play();
+      break;
+    case "d":
+      new Audio("./sounds/snare.mp3").play();
+      break;
+    case "f":
+      new Audio("./sounds/tom-1.mp3").play();
+      break;
+    case "j":
+      new Audio("./sounds/tom-2.mp3").play();
+      break;
+    case "k":
+      new Audio("./sounds/tom-3.mp3").play();
+      break;
+    case "l":
+      new Audio("./sounds/tom-4.mp3").play();
+  }
+}
+for (let i = 0; i < drums.length; i++)
+  drums[i].addEventListener("click", function () {
+    keyCheck(this.innerHTML);
+  });
+document.addEventListener("keydown", function (e) {
+  keyCheck(e.key);
+});
